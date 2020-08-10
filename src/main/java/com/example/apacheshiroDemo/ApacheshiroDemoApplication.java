01b774ff82f1d5ee622f2abfb8a99ec5fc9bc895
张虎 <george.zhang@utstarcom.cn>
2020-08-10 20:16:43 +0800

    init

---
+++ src/main/java/com/example/apacheshiroDemo/ApacheshiroDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.apacheshiroDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApacheshiroDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApacheshiroDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/apacheshiroDemo/ApacheshiroDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apacheshiroDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApacheshiroDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
