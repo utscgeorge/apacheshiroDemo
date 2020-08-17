73abf67d92dfa68c874e91d5a1bb27ef3fe42033
george.zhang <george.zhang@utstarcom.cn>
2020-08-17 23:30:42 +0800

    222

--- src/main/java/com/example/apacheshiroDemo/ApacheshiroDemoApplication.java
+++ src/main/java/com/example/apacheshiroDemo/ApacheshiroDemoApplication.java
@@ -1,9 +1,11 @@
 package com.example.apacheshiroDemo;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = {"com.example.apacheshiroDemo.mapper"})
 public class ApacheshiroDemoApplication {
 
 	public static void main(String[] args) {
